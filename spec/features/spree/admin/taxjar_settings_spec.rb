fa8803e28f6baee3e4e99ad1019f6cfe1fec9252
Noah Silvera <noah@super.gd>
2021-02-08 14:45:58 -0800

    Add basic UI for Taxjar settings in solidus admin
    This commit adds a tab to the taxes section of the solidus admin
    settings. This provides the foundation for further configuration of
    taxjar through the solidus admin.
    
    The settings should be hidden if an API key has not been provided.
    This is to conform to the certification guidelines for an official
    Taxjar extension.
    
    Co-authored-by: Nick Van Doorn <nick@super.gd>

---
+++ spec/features/spree/admin/taxjar_settings_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+RSpec.feature 'Admin TaxJar Settings', js: true do
+  stub_authorization!
+
+  background do
+    create :store, default: true
+  end
+
+  describe "Taxjar settings tab" do
+    context "Taxjar API token is set" do
+      before do
+        allow(ENV).to receive(:[]).and_return("")
+        allow(ENV).to receive(:[]).with("TAXJAR_API_KEY").and_return("API token")
+      end
+
+      it "shows a blank settings page" do
+        visit "/admin"
+        click_on "Settings"
+        expect(page).to have_content("Taxes")
+        click_on "Taxes"
+        expect(page).to have_content("TaxJar Settings")
+        click_on "TaxJar Settings"
+        expect(page).not_to have_content "You must provide a TaxJar API token"
+      end
+    end
+
+    context "Taxjar API token isn't set" do
+      it "shows a descriptive error message" do
+        visit "/admin"
+        click_on "Settings"
+        expect(page).to have_content("Taxes")
+        click_on "Taxes"
+        expect(page).to have_content("TaxJar Settings")
+        click_on "TaxJar Settings"
+        expect(page).to have_content "You must provide a TaxJar API token"
+      end
+    end
+  end
+end
